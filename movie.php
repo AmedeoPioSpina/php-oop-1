26a0ac6b2f3321e5ed6e971a82735e26cf739eae
AmedeoPioSpina <spina.amedeo.pio@gmail.com>
2024-01-25 17:05:12 +0100

    constructor and method added

--- movie.php
+++ movie.php
@@ -4,6 +4,18 @@
         public $register;
         public $relaceDate;
         public $language;
+
+        function __construct(string $name, string $register, string $relaceDate, string $language)
+        {
+             $this->name = $name;
+             $this->register = $register;
+             $this->relaceDate = $relaceDate;
+             $this->language = $language;
+        }
+
+        public function getName(){
+            return $this->name;
+        }
     }
 ?>
 
